1057796989075d133a12b8fd4cb867483f56d456
VincentB <vincent.brunelle.gm@gmail.com>
2025-09-08 12:25:36 -0400

    ADD : Solution #3110

--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    //Problem no.3110
+    public static int ScoreOfString(string s)
+    {
+        int l = s.Length;
+        int sum = 0;
+        for (int i = 0; i < l - 1; i++)
+        {
+            sum += Math.Abs(s[i] - s[i + 1]);
+        }
+        return sum;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Problem no.3110
+        Console.WriteLine("Problem #3110");
+        Console.WriteLine();
+        Console.WriteLine($"{Solution.ScoreOfString("hello")} == 13");
+        Console.WriteLine($"{Solution.ScoreOfString("zaz")} == 50");
+        
+    }
+}
